d6679ff3962d4bf3841281e87c865f363bdbbc27
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-23 22:07:07 +0545

    vehicle CRUD implementation

---
+++ app/Http/Resources/VehicleResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class VehicleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'plate_number' => $this->plate_number,
+        ];
+    }
+
+}
